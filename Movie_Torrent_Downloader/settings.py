eed3f4cb8eb595f153705ad3559df2bc647d8b10
View <xyc986327386@gmail.com>
2018-02-19 18:02:14 +0800

    v5

--- Movie_Torrent_Downloader/settings.py
+++ Movie_Torrent_Downloader/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Movie_Torrent_Downloader.urls'
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
